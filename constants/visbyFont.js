44bd48e5f63a2a10c5e35f6c9a956e83de9bb92b
mahdiyar <mahdiyar472@gmail.com>
2023-07-24 04:14:36 +0330

    add font constant

---
+++ constants/visbyFont.js
@@ -0,0 +1,10 @@
+import LocalFont from "next/font/local";
+
+const visbyFont = LocalFont({
+  src: "../public/fonts/visby round/VisbyRound.woff2",
+  variable: "--font-visby",
+  style: "normal",
+  display: "block",
+});
+
+export default visbyFont;
