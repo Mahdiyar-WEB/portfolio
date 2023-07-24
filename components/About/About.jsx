6e15118f6a1206b107a61a00c4924965cb5e8559
mahdiyar <mahdiyar472@gmail.com>
2023-07-24 04:16:28 +0330

    add about section and add css to global.css

---
+++ components/About/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+
+  const yearsOfExperience = new Date().getFullYear() - new Date("2020-03-18").getFullYear();
+
+  return (
+    <section className="text-justify mt-10">
+      <p className="relative w-96 mx-auto flex text-justify px-6 text-lg border rounded-md py-4 shadow-md shadow-yellow-800 border-yellow-600">
+      My passion for technology led me to pursue this career, and I continue to seek growth and learning with enthusiasm. I have over {yearsOfExperience} years of experience in this field.
+      </p>
+    </section>
+  );
+};
+
+export default About;
