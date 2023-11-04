13d6ffe8041f88b7915e21ae36b91a43c8a44d48
mahdiyar <mahdiyar472@gmail.com>
2023-11-04 16:27:36 +0330

    add dark mode to skills

--- components/Skills/Skill/Skill.jsx
+++ components/Skills/Skill/Skill.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Skill = ({ title, stars }) => {
+  const [isDark, setIsDark] = useState(
+    localStorage.getItem("theme") === "dark" ? true : false
+  );
   const getStars = () => {
     switch (stars) {
       case 1:
@@ -16,37 +19,33 @@ const Skill = ({ title, stars }) => {
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4 dark:fill-slate-900 fill-slate-500 text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#1F2937"
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4 dark:fill-slate-900 fill-slate-500 text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#1F2937"
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4 dark:fill-slate-900 fill-slate-500 text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#1F2937"
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4 dark:fill-slate-900 fill-slate-500 text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#1F2937"
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -75,28 +74,25 @@ const Skill = ({ title, stars }) => {
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4 dark:fill-slate-900 fill-slate-500 text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#1F2937"
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4 dark:fill-slate-900 fill-slate-500 text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#1F2937"
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4 dark:fill-slate-900 fill-slate-500 text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#1F2937"
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -134,25 +130,27 @@ const Skill = ({ title, stars }) => {
               <defs>
                 <linearGradient id="solids" x1="0%" y1="0%" x2="100%" y2="0%">
                   <stop offset="50%" stopColor="currentColor" />
-                  <stop offset="50%" stopColor="#0F172A" />
+                  <stop
+                    offset="50%"
+                    stopColor="currentColor"
+                    className="dark:text-slate-900 text-slate-500"
+                  />
                 </linearGradient>
               </defs>
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4 dark:fill-slate-900 fill-slate-500 text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#0F172A"
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4 dark:fill-slate-900 fill-slate-500 text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#0F172A"
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -190,19 +188,17 @@ const Skill = ({ title, stars }) => {
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4 dark:fill-slate-900 fill-slate-500 text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#0F172A"
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4 dark:fill-slate-900 fill-slate-500 text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#0F172A"
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -248,15 +244,18 @@ const Skill = ({ title, stars }) => {
             >
               <defs>
                 <stop offset="50%" stopColor="currentColor" />
-                <stop offset="50%" stopColor="#0F172A" />
+                <stop
+                    offset="50%"
+                    stopColor="currentColor"
+                    className="dark:text-slate-900 text-slate-500"
+                  />
               </defs>
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4 dark:fill-slate-900 fill-slate-500 text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#0F172A"
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -294,7 +293,7 @@ const Skill = ({ title, stars }) => {
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4  text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="currentColor"
@@ -303,10 +302,9 @@ const Skill = ({ title, stars }) => {
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
             <svg
-              className="w-4 h-4 text-yellow-300"
+              className="w-4 h-4 dark:fill-slate-900 fill-slate-500 text-yellow-300"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
-              fill="#0F172A"
               viewBox="0 0 22 20"
             >
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -361,7 +359,11 @@ const Skill = ({ title, stars }) => {
             >
               <defs>
                 <stop offset="50%" stopColor="currentColor" />
-                <stop offset="50%" stopColor="#0F172A" />
+                <stop
+                    offset="50%"
+                    stopColor="currentColor"
+                    className="dark:text-slate-900 text-slate-500"
+                  />
               </defs>
               <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
             </svg>
@@ -420,7 +422,7 @@ const Skill = ({ title, stars }) => {
     }
   };
   return (
-    <div className="gap-2 grid col-span-6 md:col-span-3 lg:col-span-2 lg:last:col-start-5 py-4 shadow-slate-950 rounded-md shadow-sm bg-gray-800">
+    <div className="gap-2 grid col-span-6 md:col-span-3 lg:col-span-2 lg:last:col-start-5 py-4 text-slate-600 dark:border-none shadow-md ring-1 shadow-stone-200 dark:shadow-slate-950 rounded-md dark:shadow-sm dark:text-white dark:bg-gray-800">
       <p className="text-sm md:text-lg mx-auto font-semibold">{title}</p>
       <div className="flex justify-center space-x-1">{getStars()}</div>
     </div>
