dde27b2fcf3d33a28ec5b96d6801117fa35b1778
mahdiyar <mahdiyar472@gmail.com>
2023-11-04 16:26:58 +0330

    add dark mode to footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -15,13 +15,13 @@ const Footer = () => {
   };
 
   return (
-    <footer className="bg-slate-950 px-6 pt-4 pb-2 text-white">
+    <footer className="dark:bg-slate-950 bg-zinc-50 border border-slate-300 dark:border-none px-6 pt-4 pb-2 text-white">
       <button
         onClick={handleGoTop}
         data-tooltip-id="top-tooltip"
         data-tooltip-content="Go Top"
         data-tooltip-delay-show={500}
-        className="bg-gray-800 p-2 group/button rounded-full mx-auto block -translate-y-7 ring ring-slate-950"
+        className="dark:bg-gray-800 p-2 group/button rounded-full mx-auto block -translate-y-8 dark:-translate-y-7 ring-1 bg-slate-500 ring-slate-600 dark:ring dark:ring-slate-950"
       >
         <IoIosArrowUp
           className="duration-300 group-hover/button:-translate-y-1"
@@ -33,7 +33,7 @@ const Footer = () => {
           return (
             <li key={title}>
               <Link
-                className="text-gray-400 capitalize duration-150 hover:text-gray-300 font-semibold"
+                className="dark:text-gray-400 text-slate-500 hover:text-slate-600 capitalize duration-150 dark:hover:text-gray-300 font-bold"
                 href={href}
               >
                 {title}
@@ -46,9 +46,9 @@ const Footer = () => {
         id="top-tooltip"
         place="top"
         variant="dark"
-        className="font-semibold tracking-wide"
+        className="font-semibold tracking-wide "
       />
-      <small className="text-center inline-block w-full font-sans mb-3">made with ❤️</small>
+      <small className="text-center inline-block w-full dark:text-white text-slate-700 font-sans mb-3">made with ❤️</small>
     </footer>
   );
 };
