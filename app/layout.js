831bd9de374fe5a4a3b4e2dbac35ddf23805c363
mahdiyar <mahdiyar472@gmail.com>
2023-11-04 16:26:19 +0330

    add css codes

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={`${visbyFont.variable} ${inter.className}`}>
+      <body className={`${visbyFont.variable} ${inter.className} dark:bg-slate-900 `}>
         {children}
         <Toaster />
       </body>
