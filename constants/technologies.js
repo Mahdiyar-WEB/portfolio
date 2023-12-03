93c34db72877cbfd2e2b3850c256392473969cf9
mahdiyar <mahdiyar472@gmail.com>
2023-12-03 20:52:55 +0330

    add technologies constants

---
+++ constants/technologies.js
@@ -0,0 +1,31 @@
+const technologiesAndLibraries = [
+  { name: "HTML", href: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
+  { name: "CSS", href: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+  {
+    name: "JavaScript",
+    href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  { name: "Sass", href: "https://sass-lang.com/" },
+  { name: "Material UI", href: "https://material-ui.com/" },
+  { name: "Tailwind CSS", href: "https://tailwindcss.com/" },
+  { name: "Bootstrap", href: "https://getbootstrap.com/" },
+  { name: "React", href: "https://reactjs.org/" },
+  { name: "Next.js", href: "https://nextjs.org/" },
+  { name: "TypeScript", href: "https://www.typescriptlang.org/" },
+  { name: "Webpack", href: "https://webpack.js.org/" },
+  { name: "MongoDB", href: "https://www.mongodb.com/" },
+  { name: "Node.js", href: "https://nodejs.org/" },
+  { name: "Formik", href: "https://formik.org/" },
+  { name: "Redux", href: "https://redux.js.org/" },
+  { name: "JQuery", href: "https://jquery.com/" },
+  { name: "Axios", href: "https://axios-http.com/docs/intro" },
+  { name: "Yup", href: "https://github.com/jquense/yup" },
+  { name: "React icons", href: "https://react-icons.github.io/react-icons/" },
+  { name: "React toast", href: "https://www.npmjs.com/package/react-toast-notifications" },
+  { name: "React select", href: "https://react-select.com/home" },
+  { name: "Query string", href: "https://www.npmjs.com/package/query-string" },
+  { name: "Sweet alert 2", href: "https://sweetalert2.com/download" },
+
+];
+
+export default technologiesAndLibraries;
