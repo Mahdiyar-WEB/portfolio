c2eeab26b81a8bf2a7f2f67b9e0f2fda1097c693
Mahdiyar marvi <mahdiyar472@gmail.com>
2026-08-16 05:39:57 +0330

    refactor: migrate app pages to TypeScript

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,13 @@ import "./globals.css";
 import "atropos/css";
 import "aos/dist/aos.css";
 import { Inter } from "next/font/google";
-import visbyFont from "@/constants/visbyFont";
+import visbyFont from "../constants/visbyFont";
 import { Toaster } from "react-hot-toast";
+import { ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body
